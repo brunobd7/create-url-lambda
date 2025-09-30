9c6df3ba523284f64b8b4cca58833f268b27a49b
Bruno Dantas <brunoodantas7@gmail.com>
2025-09-29 21:16:12 -0300

    feat: Add initial logic for data extraction from request and UUID url code generation.

--- src/main/java/com/dantas/createUrlShortener/Main.java
+++ src/main/java/com/dantas/createUrlShortener/Main.java
@@ -1,9 +1,37 @@
 package com.dantas.createUrlShortener;
 
 
-public class Main {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-    public String handleRequest(){
-        return "Hello World";
+import java.util.Map;
+import java.util.UUID;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> inputMapObject, Context context) {
+        String body = (String) inputMapObject.get("body");
+
+        Map<String, String> bodyMap;
+
+        try {
+            bodyMap = objectMapper.readValue(body, Map.class);
+
+        } catch (Exception exception) {
+             throw new RuntimeException("Error parsing request body! ", exception);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> responseMap = Map.of("code", shortUrlCode);
+
+        return responseMap;
     }
 }
